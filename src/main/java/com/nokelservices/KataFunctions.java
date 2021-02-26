55082a06aa9352c63416b91ba197510325294021
ecrookshanks <ed.crookshanks@gmail.com>
2021-02-25 23:34:54 -0500

    Added some additional code challenge questions - more testing needed

--- src/main/java/com/nokelservices/KataFunctions.java
+++ src/main/java/com/nokelservices/KataFunctions.java
@@ -4,6 +4,8 @@ import java.time.Duration;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Stack;
 
 public class KataFunctions {
 
@@ -47,4 +49,118 @@ public class KataFunctions {
         return HRS +":"+MINS+":" + SECS;
 
     }
+
+    public static boolean isValid(String braces) {
+        Stack<Character> items = new Stack<>();
+        Character c;
+        for(Character ch: braces.toCharArray()){
+            switch (ch){
+                case '(':
+                case '{':
+                case '[':
+                    items.push(ch);
+                    break;
+                case ')':
+                    if (items.size() == 0) return  false;
+                    c = items.peek();
+                    if ( c.equals('{') || c.equals('[')){
+                        return false;
+                    }
+                    items.pop();
+                    break;
+                case '}':
+                    if (items.size() == 0) return  false;
+                    c = items.peek();
+                    if ( c.equals('(') || c.equals('[')){
+                        return false;
+                    }
+                    items.pop();
+                    break;
+                case ']':
+                    if (items.size() == 0) return  false;
+                    c = items.peek();
+                    if ( c.equals('(') || c.equals('{')){
+                        return false;
+                    }
+                    items.pop();
+                    break;
+                default:
+                    break;
+            }
+        }
+        return items.size() == 0;
+
+    }
+
+    // note - the below is correct, but not optimized - how to further
+    // optimize it?  Maybe use toArray on the incoming List?
+    public static long howManySwaps(List<Integer> arr) {
+        int[] intArr = arr.stream().mapToInt(m -> m).toArray();
+        long totalSwaps = 0;
+        boolean didSwap = false;
+
+        int siz = intArr.length;
+        int nth, nthPlusOne, temp;
+        do{
+            didSwap = false;
+            for(int i=0;i<siz-1;i++){
+                nth = intArr[i];
+                nthPlusOne = intArr[i+1];
+                if ( nth > nthPlusOne){
+                    temp = nth;
+                    intArr[i] =  nthPlusOne;
+                    intArr[i+1] = temp;
+                    totalSwaps += 1;
+                    didSwap = true;
+                }
+            }
+        } while (didSwap == true);
+        return totalSwaps;
+    }
+
+    public static long storageOptimization(int m, int n, List<Integer> h, List<Integer> v){
+        // 3, 3, [4], [2] should return 4
+        // 2, 2, [1], [1] should return 4
+        // 3, 2, [1, 2, 3], [1, 2] should return 12
+        long result = 0;
+
+        long xConsec = 1, largestXConsec = 1;
+        long yConsec = 1, largestYConsec = 1;
+
+        // TODO: count the largest consecutive block in X
+        for(int i=1; i<h.size(); i++){
+            if(h.get(i-1) == h.get(i)-1){
+                xConsec += 1;
+            }
+            else{
+                if ( xConsec > largestXConsec){
+                    largestXConsec = xConsec;
+                }
+                xConsec = 1;
+            }
+        }
+        if (xConsec > largestXConsec) largestXConsec = xConsec;
+
+        // TODO: count the largest consecutive block in Y
+        for(int i=1; i<v.size(); i++){
+            if(v.get(i-1) == v.get(i)-1){
+                yConsec += 1;
+            }
+            else{
+                if ( yConsec > largestYConsec){
+                    largestYConsec = yConsec;
+                }
+                yConsec = 1;
+            }
+        }
+        if ( yConsec > largestYConsec) largestYConsec = yConsec;
+
+        // edge case
+        if ( largestXConsec == 1 && largestYConsec == 1) largestXConsec++;
+
+        result = (largestXConsec * largestYConsec) * 2;
+
+        return result;
+    }
+
 }
--- src/test/java/com/nokelservices/MoreKataTests.java
+++ src/test/java/com/nokelservices/MoreKataTests.java
@@ -2,7 +2,9 @@ package com.nokelservices;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -16,6 +18,41 @@ public class MoreKataTests {
         assertEquals("00:01:00", KataFunctions.makeReadable(60));
         assertEquals( "23:59:59", KataFunctions.makeReadable(86399));
         assertEquals("99:59:59", KataFunctions.makeReadable(359999));
+    }
+
+    @Test
+    public void testValid() {
+        assertEquals(true, KataFunctions.isValid("()"));
+        assertEquals(true, KataFunctions.isValid("(){}[]"));
+        assertEquals(true, KataFunctions.isValid("([{}])"));
+        // "(){}[]"   =>  True
+        //"([{}])"   =>  True
+    }
+
+    @Test
+    public void testInvalid() {
+        assertEquals(false, KataFunctions.isValid("[(])"));
+        assertEquals(false, KataFunctions.isValid("(}"));
+        assertEquals(false, KataFunctions.isValid("[(])"));
+        assertEquals(false, KataFunctions.isValid("[({})](]"));
+        assertEquals(false, KataFunctions.isValid("]()]"));
+        //"(}"       =>  False
+        //"[(])"     =>  False
+        //"[({})](]" =>  False
+    }
+
+    @Test
+    public void testSpaceOptimization(){
+        List<Integer> h = Arrays.asList(4);
+        List<Integer> v = Arrays.asList(2);
+
+        assertEquals(4, KataFunctions.storageOptimization(3, 3, h, v));
+
+        // 3, 2, [1, 2, 3], [1, 2] should return 12
+        h = Arrays.asList(1, 2, 3);
+        v = Arrays.asList(1, 2);
+        assertEquals(12, KataFunctions.storageOptimization(3, 3, h, v));
+
     }
 
 }
